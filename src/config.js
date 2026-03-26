b5006600d5399783cebb5a9112dce73c718fde97
Meetthumar02 <134767714+Meetthumar02@users.noreply.github.com>
2026-03-26 09:37:33 +0530

    - remove URL for render

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // const BASE_URI = "http://localhost:8090";
-// const BASE_URI = "https://dev.airvlt.com";
-const BASE_URI = "https://airvault-savan.onrender.com";
+const BASE_URI = "https://dev.airvlt.com";
+// const BASE_URI = "https://airvault-savan.onrender.com";
 
 export default BASE_URI;
